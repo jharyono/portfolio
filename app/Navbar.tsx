6ad64a6d46d6aae9dcabe58c0a89601c1bc4bdf8
Joni Haryono <jharyono@gmail.com>
2024-07-13 05:42:54 +0700

    add navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,5 +1,19 @@
+import { navLinks } from "./config"
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="flex justify-between">
+      <div>Joni Haryono</div>
+      <div className="flex space-x-4">
+        <ul className="flex space-x-4">
+          {navLinks.map((link, index) => (
+            <li key={index}>{link.name}</li>
+          ))}
+        </ul>
+        <div>Resume</div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
---
+++ app/config.ts
@@ -0,0 +1,45 @@
+export const email = "jharyono@gmail.com"
+
+export const socialMedia = [
+  {
+    name: "GitHub",
+    url: "https://github.com/jharyono",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/jharyono",
+  },
+  {
+    name: "X",
+    url: "https://twitter.com/bchiang7",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/bchiang7",
+  },
+]
+
+export const navLinks = [
+  {
+    name: "About",
+    url: "/#about",
+  },
+  {
+    name: "Experience",
+    url: "/#jobs",
+  },
+  {
+    name: "Project",
+    url: "/#projects",
+  },
+  {
+    name: "Contact",
+    url: "/#contact",
+  },
+]
+
+export const colors = {
+  green: "#64ffda",
+  navy: "#0a192f",
+  darkNavy: "#020c1b",
+}
